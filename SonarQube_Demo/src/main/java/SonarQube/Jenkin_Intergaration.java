161d477e7db57ac085651f934f41cb6433b0ed5e
suneelsirimisetti <45848900+suneelsirimisetti@users.noreply.github.com>
2019-10-19 20:31:04 +0530

    Commit Demo project

---
+++ SonarQube_Demo/src/main/java/SonarQube/Jenkin_Intergaration.java
@@ -0,0 +1,16 @@
+package SonarQube;
+
+public class Jenkin_Intergaration {
+
+	public static void main(String[] args) {
+	System.out.println("Jenkins integaration with SonarCube");
+	System.out.println("Jenkins integaration with SonarCube");
+	test();
+	}
+	
+	public static void test()
+	{
+		System.out.println("calling");
+	}
+
+}
